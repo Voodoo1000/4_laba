36e533563ed4df6f2cc074593dbaa0a92100f061
Voodoo <vladbutakov08@gmail.com>
2024-04-08 14:56:12 +0800

    Добавление комментариев

--- Drinks.cs
+++ Drinks.cs
@@ -6,87 +6,109 @@ using System.Threading.Tasks;
 
 namespace _4_laba
 {
+    // Базовый класс для напитков
     public class Drink
     {
-        public int volume = 0;
-        public static Random rnd = new Random();
+        public int volume = 0; // Объем напитка
+        public static Random rnd = new Random(); // Статический генератор случайных чисел
+
+        // Виртуальный метод для получения информации о напитке
         public virtual String GetInfo()
         {
-            var str = String.Format("\nОбъем: {0}", this.volume);
+            var str = String.Format("\nОбъем: {0}", this.volume); // Форматирование строки с информацией о объеме
             return str;
         }
     }
 
+    // Перечисление для типов фруктов
     public enum Fruit { apricot, orange, grape, pomegranate, plum, apple };
-    public class Juice : Drink // Сок
+
+    // Класс для сока, наследуется от Drink
+    public class Juice : Drink
     {
         public Fruit fruit = Fruit.apple; // Фрукт
-        public int countPulp = 0; // Количество  мякоти
+        public int countPulp = 0; // Количество мякоти
 
+        // Переопределение метода GetInfo для вывода информации о соке
         public override string GetInfo()
         {
-            var str = "Сок: ";
-            str += base.GetInfo();
-            str += String.Format("\nФрукт: {0}", this.fruit);
-            str += String.Format("\nКоличество мякоти: {0}", this.countPulp);
+            var str = "Сок: "; // Заголовок для сока
+            str += base.GetInfo(); // Получаем информацию из базового класса
+            str += String.Format("\nФрукт: {0}", this.fruit); // Добавляем информацию о фрукте
+            str += String.Format("\nКоличество мякоти: {0}", this.countPulp); // Добавляем информацию о мякоти
             return str;
         }
 
+        // Метод для генерации случайного сока
         public static Juice Generate()
         {
             return new Juice
             {
-                volume = rnd.Next() % 500,
-                countPulp = 200 + rnd.Next() % 2000,
-                fruit = (Fruit)rnd.Next(6)
+                volume = rnd.Next() % 500, // Случайный объем от 0 до 499
+                countPulp = 200 + rnd.Next() % 2001, // Случайное количество мякоти от 200 до 2200
+                fruit = (Fruit)rnd.Next(6) // Случайный фрукт из перечисления
             };
         }
     }
 
+    // Перечисление для типов газировок
     public enum TypeSoda { cola, fanta, sprite, pepsi, mirinda }
-    public class Soda : Drink // Газировка
+
+    // Класс для газировки, наследуется от Drink
+    public class Soda : Drink
     {
-        public TypeSoda type = TypeSoda.cola; // Тип
+        public TypeSoda type = TypeSoda.cola; // Тип газировки
         public int countBubbles = 0; // Количество пузыриков
+
+        // Переопределение метода GetInfo для вывода информации о газировке
         public override string GetInfo()
         {
-            var str = "Газировка: ";
-            str += base.GetInfo();
-            str += String.Format("\nТип: {0}", this.type);
-            str += String.Format("\nКоличество пузыриков: {0}", this.countBubbles);
+            var str = "Газировка: "; // Заголовок для газировки
+            str += base.GetInfo(); // Получаем информацию из базового класса
+            str += String.Format("\nТип: {0}", this.type); // Добавляем информацию о типе
+            str += String.Format("\nКоличество пузыриков: {0}", this.countBubbles); // Добавляем информацию о пузырьках
             return str;
         }
+
+        // Метод для генерации случайной газировки
         public static Soda Generate()
         {
             return new Soda
             {
-                volume = rnd.Next() % 500,
-                countBubbles = 500 + rnd.Next() % 3000,
-                type = (TypeSoda)rnd.Next(5)
+                volume = rnd.Next() % 500, // Случайный объем от 0 до 499
+                countBubbles = 500 + rnd.Next() % 3001, // Случайное количество пузырьков от 500 до 3500
+                type = (TypeSoda)rnd.Next(5) // Случайный тип из перечисления
             };
         }
     }
-    
+
+    // Перечисление для типов алкоголя
     public enum TypeAlcohol { absinthe, gin, vodka, whiskey, rum, cognac, wine }
-    public class Alcohol : Drink // Алкоголь
+
+    // Класс для алкоголя, наследуется от Drink
+    public class Alcohol : Drink
     {
-        public TypeAlcohol type = TypeAlcohol.gin; // Тип
-        public int strength = 0; //Крепкость алкоголя
+        public TypeAlcohol type = TypeAlcohol.gin; // Тип алкоголя
+        public int strength = 0; // Крепкость алкоголя
+
+        // Переопределение метода GetInfo для вывода информации об алкоголе
         public override string GetInfo()
         {
-            var str = "Алкоголь:";
-            str += base.GetInfo();
-            str += String.Format("\nТип: {0}", this.type);
-            str += String.Format("\nКрепкость: {0}", this.strength);
+            var str = "Алкоголь:"; // Заголовок для алкоголя
+            str += base.GetInfo(); // Получаем информацию из базового класса
+            str += String.Format("\nТип: {0}", this.type); // Добавляем информацию о типе
+            str += String.Format("\nКрепкость: {0}", this.strength); // Добавляем информацию о крепкости
             return str;
         }
+
+        // Метод для генерации случайного алкоголя
         public static Alcohol Generate()
         {
             return new Alcohol
             {
-                volume = rnd.Next() % 500,
-                strength = 15 + rnd.Next() % 80,
-                type = (TypeAlcohol)rnd.Next(7)
+                volume = rnd.Next() % 500, // Случайный объем от 0 до 499
+                strength = 15 + rnd.Next() % 66, // Случайная крепкость от 15 до 80
+                type = (TypeAlcohol)rnd.Next(7) // Случайный тип из перечисления
             };
         }
     }
--- Form1.cs
+++ Form1.cs
@@ -1,80 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
 namespace _4_laba
 {
     public partial class Form1 : Form
     {
+        // Список напитков
         List<Drink> drinksList = new List<Drink>();
 
+        // Конструктор формы
         public Form1()
         {
             InitializeComponent();
-            ShowInfo();
+            ShowInfo(); // Отображение информации о напитках
         }
 
+        // Обработчик нажатия кнопки "Перезаполнить"
         private void btnRefill_Click(object sender, EventArgs e)
         {
-            this.drinksList.Clear();
+            this.drinksList.Clear(); // Очистка списка напитков
 
             var rnd = new Random();
 
+            // Генерация 10 случайных напитков
             for (var i = 0; i < 10; ++i)
             {
                 switch (rnd.Next() % 3)
                 {
                     case 0:
-                        this.drinksList.Add(Juice.Generate());
+                        this.drinksList.Add(Juice.Generate()); // Добавление случайного сока
                         break;
                     case 1:
-                        this.drinksList.Add(Soda.Generate());
+                        this.drinksList.Add(Soda.Generate()); // Добавление случайной газировки
                         break;
                     case 2:
-                        this.drinksList.Add(Alcohol.Generate());
+                        this.drinksList.Add(Alcohol.Generate()); // Добавление случайного алкоголя
                         break;
                 }
             }
-            ShowInfo();
+            ShowInfo(); // Отображение информации о напитках
         }
 
+        // Метод для отображения информации о напитках
         private void ShowInfo()
         {
             int juiceCount = 0;
             int sodaCount = 0;
             int alcoholCount = 0;
 
-            foreach (var fruit in this.drinksList)
+            // Подсчет количества каждого типа напитка
+            foreach (var drink in this.drinksList)
             {
-                if (fruit is Juice)
+                if (drink is Juice)
                 {
                     juiceCount += 1;
                 }
-                else if (fruit is Soda)
+                else if (drink is Soda)
                 {
                     sodaCount += 1;
                 }
-                else if (fruit is Alcohol)
+                else if (drink is Alcohol)
                 {
                     alcoholCount += 1;
                 }
             }
 
-
+            // Отображение информации о количестве каждого типа напитка
             txtInfo.Text = "Сок\t\tГазировка\tАлкоголь";
             txtInfo.Text += "\n";
             txtInfo.Text += String.Format("{0}\t\t{1}\t\t{2}", juiceCount, sodaCount, alcoholCount);
         }
 
+        // Обработчик нажатия кнопки "Взять"
         private void btnGet_Click(object sender, EventArgs e)
         {
             if (this.drinksList.Count == 0)
             {
-                txtOut.Text = "Пусто Q_Q";
+                txtOut.Text = "Пусто Q_Q"; // Вывод сообщения о пустом списке напитков
                 return;
             }
-            var drink = this.drinksList[0];
-            this.drinksList.RemoveAt(0);
+            var drink = this.drinksList[0]; // Получение первого напитка из списка
+            this.drinksList.RemoveAt(0); // Удаление первого напитка из списка
 
-            txtOut.Text = drink.GetInfo();
+            txtOut.Text = drink.GetInfo(); // Отображение информации о напитке
 
-            ShowInfo();
+            ShowInfo(); // Отображение информации о напитках
         }
     }
 }
