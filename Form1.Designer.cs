b8567b11113e10fea09cbccfabd87225892f11bc
Voodoo <vladbutakov08@gmail.com>
2024-04-08 12:12:37 +0800

    Создание интерфейса

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnRefill = new Button();
+            txtInfo = new RichTextBox();
+            txtOut = new RichTextBox();
+            btnGet = new Button();
             SuspendLayout();
+            // 
+            // btnRefill
+            // 
+            btnRefill.Location = new Point(12, 12);
+            btnRefill.Name = "btnRefill";
+            btnRefill.Size = new Size(366, 29);
+            btnRefill.TabIndex = 0;
+            btnRefill.Text = "Перезаполнить";
+            btnRefill.UseVisualStyleBackColor = true;
+            // 
+            // txtInfo
+            // 
+            txtInfo.BorderStyle = BorderStyle.None;
+            txtInfo.Location = new Point(12, 47);
+            txtInfo.Name = "txtInfo";
+            txtInfo.ReadOnly = true;
+            txtInfo.Size = new Size(366, 66);
+            txtInfo.TabIndex = 1;
+            txtInfo.Text = "";
+            // 
+            // txtOut
+            // 
+            txtOut.Location = new Point(12, 119);
+            txtOut.Name = "txtOut";
+            txtOut.Size = new Size(233, 157);
+            txtOut.TabIndex = 2;
+            txtOut.Text = "";
+            // 
+            // btnGet
+            // 
+            btnGet.Location = new Point(251, 119);
+            btnGet.Name = "btnGet";
+            btnGet.Size = new Size(127, 157);
+            btnGet.TabIndex = 3;
+            btnGet.Text = "Взять";
+            btnGet.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(392, 288);
+            Controls.Add(btnGet);
+            Controls.Add(txtOut);
+            Controls.Add(txtInfo);
+            Controls.Add(btnRefill);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnRefill;
+        private RichTextBox txtInfo;
+        private RichTextBox txtOut;
+        private Button btnGet;
     }
 }
